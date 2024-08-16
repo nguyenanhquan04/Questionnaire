952a91732067a17509017e9c9c9af268ab36f277
nguyenanhquan04 <fpskirito2004@gmail.com>
2024-08-16 16:10:03 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <img src="./public/logo.jpg" alt="Logo" />
+        <img src="/logo.jpg" alt="Logo" />
       </div>
       <nav>
         <ul>
