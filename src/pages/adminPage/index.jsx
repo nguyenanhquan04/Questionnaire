ad126e16682c4159e28e360545abfcd81d8e34e0
Nguyenvkxse183970 <nguyenvkxse183970@fpt.edu.vn>
2024-08-20 16:07:12 +0700

    update admin

--- src/pages/adminPage/index.jsx
+++ src/pages/adminPage/index.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Card, Button, Row, Col, Typography, Modal, Input, Select } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
-import ToastNotification, { notifySuccess } from '../../components/Toastify'; // Corrected import
-import { answerQuestion, deleteQuestion } from '../../slices/questionsSlice'; // Import the actions from your slice
-import '../adminPage/index.scss'; // Adjust your CSS import if needed
+import ToastNotification, { notifySuccess } from '../../components/Toastify';
+import { answerQuestion, deleteQuestion } from '../../slices/questionsSlice';
+import '../adminPage/index.scss';
 
-const { Text, Title } = Typography;
+const { Text } = Typography;
 const { TextArea } = Input;
 const { Option } = Select;
 
@@ -18,7 +18,7 @@ const AdminPage = () => {
   const [isDeleteConfirmVisible, setIsDeleteConfirmVisible] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(null);
   const [answer, setAnswer] = useState('');
-  const [filter, setFilter] = useState('all'); // Filter state
+  const [filter, setFilter] = useState('all');
 
   const showAnswerModal = (question) => {
     setCurrentQuestion(question);
@@ -27,7 +27,7 @@ const AdminPage = () => {
 
   const handleOk = () => {
     if (answer) {
-      dispatch(answerQuestion({ id: currentQuestion.id, answer })); // Dispatch the action
+      dispatch(answerQuestion({ id: currentQuestion.id, answer }));
       notifySuccess('Question answered successfully');
       setIsModalOpen(false);
       setAnswer('');
@@ -45,16 +45,15 @@ const AdminPage = () => {
   };
 
   const handleDelete = () => {
-    dispatch(deleteQuestion(currentQuestion.id)); // Dispatch the action
+    dispatch(deleteQuestion(currentQuestion.id));
     setIsDeleteConfirmVisible(false);
-    notifySuccess('Question deleted successfully'); // Notify on successful delete
+    notifySuccess('Question deleted successfully');
   };
 
   const handleFilterChange = (value) => {
     setFilter(value);
   };
 
-  // Filter questions based on the selected filter
   const filteredQuestions = questions.filter(question => {
     if (filter === 'answered') {
       return question.answer;
@@ -66,9 +65,7 @@ const AdminPage = () => {
   });
 
   return (
-    <div className="admin-page" style={{ margin: '20px auto', padding: '20px', background: '#fff' }}>
-      {/* <Title level={2} style={{ color: 'gray' }}>Admin: Answer Questions</Title> */}
-
+    <div className="admin-page">
       <div style={{ marginBottom: 16, textAlign: 'center' }}>
         <Select defaultValue="all" onChange={handleFilterChange} style={{ width: 200 }} className="blue-select">
           <Option value="all">All Questions</Option>
@@ -77,50 +74,52 @@ const AdminPage = () => {
         </Select>
       </div>
 
-      <Row gutter={[16, 16]} justify="center">
-        {filteredQuestions.map((question) => (
-          <Col key={question.id} xs={24} sm={12} md={6}>
-            <Card
-              size="small"
-              hoverable
-              style={{
-                width: 240,
-                margin: '10px',
-                background: ['#ffc', '#cfc', '#ccf'][question.id % 3],
-                transform: `rotate(${(question.id % 2 === 0 ? 4 : -4) + (question.id % 3 === 0 ? -3 : 0)}deg)`,
-                boxShadow: '5px 5px 7px rgba(33,33,33,.7)',
-                transition: 'transform .15s linear',
-              }}
-              title={
-                <div style={{ position: 'relative' }}>
-                  <Text style={{ color: 'black' }}>{question.text}</Text>
-                  <DeleteOutlined
-                    style={{
-                      position: 'absolute',
-                      top: 0,
-                      right: 0,
-                      color: 'red',
-                      fontSize: '16px',
-                      cursor: 'pointer',
-                    }}
-                    onClick={() => showDeleteConfirm(question)}
-                  />
+      <div className="card-container">
+        <Row gutter={[16, 16]} justify="center">
+          {filteredQuestions.map((question) => (
+            <Col key={question.id} xs={24} sm={12} md={6}>
+              <div className="pin-icon">📌</div>
+              <Card
+                size="small"
+                hoverable
+                style={{
+                  width: 240,
+                  margin: '10px',
+                  background: ['#ffc', '#cfc', '#ccf'][question.id % 3],
+                  transform: `rotate(${(question.id % 2 === 0 ? 4 : -4) + (question.id % 3 === 0 ? -3 : 0)}deg)`,
+                  boxShadow: '5px 5px 7px rgba(33,33,33,.7)',
+                  transition: 'transform .15s linear',
+                }}
+                title={
+                  <div style={{ position: 'relative' }}>
+                    <Text style={{ color: 'black' }}>{question.text}</Text>
+                    <DeleteOutlined
+                      style={{
+                        position: 'absolute',
+                        top: 0,
+                        right: 0,
+                        color: 'red',
+                        fontSize: '16px',
+                        cursor: 'pointer',
+                      }}
+                      onClick={() => showDeleteConfirm(question)}
+                    />
+                  </div>
+                }
+              >
+                <Text strong style={{ color: 'black' }}>Answer:</Text> 
+                <Text style={{ color: 'black' }}>{question.answer || 'No answer yet'}</Text>
+                <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: 10 }}>
+                  <Button type="primary" onClick={() => showAnswerModal(question)}>
+                    Answer
+                  </Button>
                 </div>
-              }
-            >
-              <Text strong style={{ color: 'black' }}>Answer:</Text> 
-              <Text style={{ color: 'black' }}>{question.answer || 'No answer yet'}</Text>
-              <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: 10 }}>
-                <Button type="primary" onClick={() => showAnswerModal(question)}>
-                  Answer
-                </Button>
-              </div>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
 
-      {/* Modal for answering a question */}
       <Modal
         title="Provide an Answer"
         open={isModalOpen}
@@ -143,7 +142,6 @@ const AdminPage = () => {
         )}
       </Modal>
 
-      {/* Confirmation modal for deletion */}
       <Modal
         title="Are you sure you want to delete this question?"
         open={isDeleteConfirmVisible}
@@ -155,7 +153,6 @@ const AdminPage = () => {
         <p style={{ color: 'black' }}>{currentQuestion?.text}</p>
       </Modal>
 
-      {/* Toast Notification Container */}
       <ToastNotification />
     </div>
   );
